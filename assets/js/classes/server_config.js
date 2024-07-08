69ca9c6f0e94c10dd7e9490ddeadfe655bc48b82
Jose F Rivas C <josefelixrc7@gmail.com>
2024-07-08 14:20:34 -0400

    refactor: Modify server config parameters

--- assets/js/classes/server_config.js
+++ assets/js/classes/server_config.js
@@ -4,7 +4,7 @@ class ServerConfig
     constructor()
     {
         this.url_production = "https://pedidosturpial.com";
-        this.url_development = "https://127.0.0.1:8080";
+        this.url_development = "https://localhost:3000";
         this.api_url = `${this.url_production}/api`;
         this.host_url = this.url_production;
 
@@ -13,6 +13,10 @@ class ServerConfig
             case "pedidosturpial.com":
                 this.api_url = `${this.url_production}/api`;
                 this.host_url = this.url_production;
+            break;
+            case "localhost":
+                this.api_url = `${this.url_development}/api`;
+                this.host_url = this.url_development;
             break;
             case "127.0.0.1":
                 this.api_url = `${this.url_development}/api`;
