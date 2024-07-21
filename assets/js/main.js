9f31c4d9875054b3d23776e2f809c1617b621463
Jose F Rivas C <josefelixrc7@gmail.com>
2024-07-20 20:53:08 -0400

    feat: Add main.js file

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+
+var server_config = new wtools.ServerConfig(
+    url_production = new wtools.SeverConfigPair("structbi.com", "https://structbi.com")
+    ,url_development = new wtools.SeverConfigPair("localhost", "https://localhost:3000")
+    ,api_url = "/api"
+);
