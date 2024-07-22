a97c061e5b908ee24b1239fca16da017c79d0ca4
Jose F Rivas C <josefelixrc7@gmail.com>
2024-07-22 16:40:20 -0400

    feat: Improve elements identificators

--- assets/js/handlers/forms_handler.js
+++ assets/js/handlers/forms_handler.js
@@ -5,7 +5,7 @@ $(function()
     const form_read = () =>
     {
         // Wait animation
-        let wait = new wtools.ElementState('.table_forms_all_notification', true, 'block', new wtools.WaitAnimation().for_block);
+        let wait = new wtools.ElementState('#component_forms_read .notifications', true, 'block', new wtools.WaitAnimation().for_block);
 
         // Request
         new wtools.Request(server_config.current.api + "/forms/read").Exec_((response_data) =>
@@ -20,8 +20,8 @@ $(function()
 
             // Results elements creator
             wait.Off_();
-            $('.table_forms_all tbody').html('');
-            new wtools.UIElementsCreator('.table_forms_all tbody', response_data.body.data).Build_((row) =>
+            $('#component_forms_read table tbody').html('');
+            new wtools.UIElementsCreator('#component_forms_read table tbody', response_data.body.data).Build_((row) =>
             {
                 let elements = [
                     `<th scope="row"><a class="text-dark" href="../form/?form=${row.identifier}">${row.identifier}</a></th>`
@@ -37,12 +37,12 @@ $(function()
                             </a>
                             <ul class="dropdown-menu">
                                 <li>
-                                    <a class="dropdown-item form_modify_button" form_id="${row.id}" form_name="${row.name}">
+                                    <a class="dropdown-item modify" form_id="${row.id}" form_name="${row.name}">
                                         Editar
                                     </a>
                                 </li>
                                 <li>
-                                    <a class="dropdown-item form_delete_button" form_id="${row.id}" form_name="${row.name}">
+                                    <a class="dropdown-item delete" form_id="${row.id}" form_name="${row.name}">
                                         Eliminar
                                     </a>
                                 </li>
@@ -56,32 +56,32 @@ $(function()
         });
     };
     form_read();
-    $('.button_forms_read').click(() => form_read());
+    $('#component_forms_read .update').click(() => form_read());
     
     // Add
-    $('.form_forms_add').submit((e) =>
+    $('#component_forms_add form').submit((e) =>
     {
         e.preventDefault();
 
         // Wait animation
-        let wait = new wtools.ElementState('.form_forms_add button[type=submit]', true, 'button', new wtools.WaitAnimation().for_button);
+        let wait = new wtools.ElementState('#component_forms_add form button[type=submit]', true, 'button', new wtools.WaitAnimation().for_button);
 
         // Form check
         const check = new wtools.FormChecker(e.target).Check_();
         if(!check)
         {
-            $('.form_forms_add .notification').html('');
+            $('#component_forms_add .notifications').html('');
             wait.Off_();
-            new wtools.Notification('WARNING', 5000, '.form_add .notification').Show_('Hay campos inv&aacute;lidos.');
+            new wtools.Notification('WARNING', 5000, '#component_forms_add .notifications').Show_('Hay campos inv&aacute;lidos.');
             return;
         }
 
         // Data collection
         const data = new FormData();
-        data.append("identifier", $('.form_forms_add input[name="form_identifier"]').val());
-        data.append("name", $('.form_forms_add input[name="form_name"]').val());
-        data.append("state", $('.form_forms_add select[name="form_state"]').val());
-        data.append("description", $('.form_forms_add textarea[name="form_description"]').val());
+        data.append("identifier", $('#component_forms_add input[name="identifier"]').val());
+        data.append("name", $('#component_forms_add input[name="name"]').val());
+        data.append("state", $('#component_forms_add select[name="state"]').val());
+        data.append("description", $('#component_forms_add textarea[name="description"]').val());
 
         // Request
         new wtools.Request(server_config.current.api + "/forms/add", "POST", data, false).Exec_((response_data) =>
@@ -92,18 +92,18 @@ $(function()
             if(response_data.status == 200)
             {
                 new wtools.Notification('SUCCESS').Show_('Formulario creado exitosamente.');
-                $('#forms_add').modal('hide');
+                $('#component_forms_add').modal('hide');
                 form_read();
             }
             else
             {
-                new wtools.Notification('ERROR', 0, '.form_forms_add .notification').Show_('Hubo un error al crear el formulario: ' + response_data.body.message);
+                new wtools.Notification('ERROR', 0, '#component_forms_add .notifications').Show_('Hubo un error al crear el formulario: ' + response_data.body.message);
             }
         });
     });
 
     // Modify
-    $(document).on("click", '.form_modify_button', (e) =>
+    $(document).on("click", '#component_forms_read table .modify', (e) =>
     {
         e.preventDefault();
 
@@ -115,7 +115,7 @@ $(function()
         const form_name = $(e.target).attr('form_name');
 
         // Setup form to modify
-        $('.form_forms_modify strong.form_name').html(form_name);
+        $('#component_forms_modify .modal-title strong.header').html(form_name);
         
         // Read form to modify
         const data = [{"action_id": "a1", "parameters":[{"name": "form_id", "value": form_id}]}]
@@ -123,7 +123,7 @@ $(function()
         {
             if(response_data.status != 200)
             {
-                $(':input','.form_forms_modify')
+                $(':input','#component_forms_modify form')
                     .not(':button, :submit, :reset, :hidden')
                     .val('')
                     .prop('checked', false)
@@ -133,40 +133,40 @@ $(function()
                 return;
             }
             
-            $('.form_forms_modify input[name="form_modify_identifier"]').val(response_data.body.data[0].identifier);
-            $('.form_forms_modify input[name="form_modify_name"]').val(response_data.body.data[0].name);
-            $('.form_forms_modify select[name="form_modify_state"]').val(response_data.body.data[0].state);
-            $('.form_forms_modify textarea[name="form_modify_description"]').val(response_data.body.data[0].description);
+            $('#component_forms_modify input[name="identifier"]').val(response_data.body.data[0].identifier);
+            $('#component_forms_modify input[name="name"]').val(response_data.body.data[0].name);
+            $('#component_forms_modify select[name="state"]').val(response_data.body.data[0].state);
+            $('#component_forms_modify textarea[name="description"]').val(response_data.body.data[0].description);
 
             wait.Off_();
-            $('#forms_modify').modal('show');
+            $('#component_forms_modify').modal('show');
         });
 
         // Modify form
-        $('.form_forms_modify').submit((e) =>
+        $('#component_forms_modify form').submit((e) =>
         {
             e.preventDefault();
 
             // Wait animation
-            let wait = new wtools.ElementState('.form_forms_modify button[type=submit]', true, 'button', new wtools.WaitAnimation().for_button);
+            let wait = new wtools.ElementState('#component_forms_modify form button[type=submit]', true, 'button', new wtools.WaitAnimation().for_button);
 
             // Form check
             const check = new wtools.FormChecker(e.target).Check_();
             if(!check)
             {
                 wait.Off_();
-                $('.forms_modify .notification').html('');
-                new wtools.Notification('WARNING', 5000, '.forms_modify .notification').Show_('Hay campos inv&aacute;lidos.');
+                $('#component_forms_modify .notifications').html('');
+                new wtools.Notification('WARNING', 5000, '#component_forms_modify .notifications').Show_('Hay campos inv&aacute;lidos.');
                 return;
             }
 
             // Data collection
             const new_data = new FormData();
             new_data.append("id", form_id);
-            new_data.append("identifier", $('.form_forms_modify input[name="form_modify_identifier"]').val());
-            new_data.append("name", $('.form_forms_modify input[name="form_modify_name"]').val());
-            new_data.append("state", $('.form_forms_modify select[name="form_modify_state"]').val());
-            new_data.append("description", $('.form_forms_modify textarea[name="form_modify_description"]').val());
+            new_data.append("identifier", $('#component_forms_modify input[name="identifier"]').val());
+            new_data.append("name", $('#component_forms_modify input[name="name"]').val());
+            new_data.append("state", $('#component_forms_modify select[name="state"]').val());
+            new_data.append("description", $('#component_forms_modify textarea[name="description"]').val());
 
             // Request
             new wtools.Request(server_config.current.api + "/forms/modify", "PUT", new_data, false).Exec_((response_data) =>
@@ -175,19 +175,19 @@ $(function()
                 if(response_data.status == 200)
                 {
                     new wtools.Notification('SUCCESS').Show_('Formulario modificado exitosamente.');
-                    $('#forms_modify').modal('hide');
+                    $('#component_forms_modify').modal('hide');
                     form_read();
                 }
                 else
                 {
-                    new wtools.Notification('ERROR', 0, '.form_forms_modify .notification').Show_('Hubo un error al modificar el formulario: ' + response_data.body.message);
+                    new wtools.Notification('ERROR', 0, '.#component_forms_modify .notifications').Show_('Hubo un error al modificar el formulario: ' + response_data.body.message);
                 }
             });
         });
     });
 
     // Delete
-    $(document).on("click", '.form_delete_button', (e) =>
+    $(document).on("click", '#component_forms_read .delete', (e) =>
     {
         e.preventDefault();
 
@@ -199,18 +199,19 @@ $(function()
         const form_name = $(e.target).attr('form_name');
 
         // Setup form to delete
-        $('.form_forms_delete strong.form_id').html(form_id);
-        $('.form_forms_delete strong.form_name').html(form_name);
-        $('#forms_delete').modal('show');
+        $('#component_forms_delete strong.header').html(form_name);
+        $('#component_forms_delete strong.id').html(form_id);
+        $('#component_forms_delete strong.name').html(form_name);
+        $('#component_forms_delete').modal('show');
         wait.Off_();
 
         // Delete form
-        $('.form_forms_delete').submit((e) =>
+        $('#component_forms_delete form').submit((e) =>
         {
             e.preventDefault();
 
             // Wait animation
-            let wait = new wtools.ElementState('.form_forms_delete button[type=submit]', true, 'button', new wtools.WaitAnimation().for_button);
+            let wait = new wtools.ElementState('#component_forms_delete form button[type=submit]', true, 'button', new wtools.WaitAnimation().for_button);
 
             // Request
             new wtools.Request(server_config.current.api + `/forms/delete?id=${form_id}`, "DEL").Exec_((response_data) =>
@@ -221,11 +222,11 @@ $(function()
                 {
                     form_read();
                     new wtools.Notification('SUCCESS').Show_('Formulario eliminado exitosamente.');
-                    $('#forms_delete').modal('hide');
+                    $('#component_forms_delete').modal('hide');
                 }
                 else
                 {
-                    new wtools.Notification('ERROR', 0, '.form_forms_delete .notification').Show_('Hubo un error al eliminar el formulario: ' + response_data.body.message);
+                    new wtools.Notification('ERROR', 0, '#component_forms_delete .notifications').Show_('Hubo un error al eliminar el formulario: ' + response_data.body.message);
                 }
             });
         });
