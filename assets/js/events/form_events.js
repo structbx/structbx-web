d8ebc892151b0edc7ef547dd7805fcd14ea439ff
Jose F Rivas C <josefelixrc7@gmail.com>
2024-09-12 14:03:37 -0400

    feat: Add Form events

---
+++ assets/js/events/form_events.js
@@ -0,0 +1,30 @@
+
+$(function ()
+{
+    // Verify Session
+    /*let verify_session = () =>
+    {
+        // Wait animation
+        let wait = new wtools.ElementState('#wait_animation_page', true, 'block', new wtools.WaitAnimation().for_page);
+
+        // Request
+        new wtools.Request(server_config.current.api + "/system/login", "POST").Exec_((response_data) =>
+        {
+            if(response_data.status != 200)
+            {
+                window.location.href = "../login/";
+                return;
+            }
+
+            wait.Off_();
+        });
+    };
+    verify_session();*/
+
+    // Elements
+        new Sidebars().SidebarMenuForm_();
+        new Headers().HeaderForm_();
+        new Footers().Footer_();
+        new wtools.MenuManager('.sidebar_menu', true);
+        
+});
