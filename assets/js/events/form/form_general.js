b1e69851feb0e31ded1a1f77aeb1124a410b17d6
Jose F Rivas C <josefelixrc7@gmail.com>
2024-09-18 14:56:16 -0400

    feat: Add functions to drag and drop items

--- assets/js/events/form/form_general.js
+++ assets/js/events/form/form_general.js
@@ -26,5 +26,44 @@ $(function ()
         new Headers().HeaderForm_();
         new Footers().Footer_();
         new wtools.MenuManager('.sidebar_menu', true);
-        
+    
+    // Drag and drop Interface Design
+    $("#component_interface_design_columns, #component_interface_design_layout").sortable({connectWith: ".connectedSortable"})
+    .disableSelection();
+
+    // Click on Drag and drop item
+    $('.form-design-item').click((e) =>
+    {
+        const element = e.currentTarget.id;
+        $('#component_interface_design_item_design input[name="element"]').val(element);
+        for(let html_class of $(`#${element}`)[0].classList)
+        {
+            if(html_class == "col-md-12") $('#component_interface_design_item_design select[name="size"]').val("12");
+            if(html_class == "col-md-11") $('#component_interface_design_item_design select[name="size"]').val("11");
+            if(html_class == "col-md-10") $('#component_interface_design_item_design select[name="size"]').val("10");
+            if(html_class == "col-md-9") $('#component_interface_design_item_design select[name="size"]').val("9");
+            if(html_class == "col-md-8") $('#component_interface_design_item_design select[name="size"]').val("8");
+            if(html_class == "col-md-7") $('#component_interface_design_item_design select[name="size"]').val("7");
+            if(html_class == "col-md-6") $('#component_interface_design_item_design select[name="size"]').val("6");
+            if(html_class == "col-md-5") $('#component_interface_design_item_design select[name="size"]').val("5");
+            if(html_class == "col-md-4") $('#component_interface_design_item_design select[name="size"]').val("4");
+            if(html_class == "col-md-3") $('#component_interface_design_item_design select[name="size"]').val("3");
+            if(html_class == "col-md-2") $('#component_interface_design_item_design select[name="size"]').val("2");
+            if(html_class == "col-md-1") $('#component_interface_design_item_design select[name="size"]').val("1");
+        }
+    });
+
+    // Change size of Drag and drop item
+    $('#component_interface_design_item_design select[name="size"]').change((e) =>
+    {
+        const element = $('#component_interface_design_item_design input[name="element"]').val();
+        const new_size = $(e.currentTarget).val();
+
+        // Remove classes
+        for(let i = 1; i < 13; i++)
+            $(`#${element}`).removeClass(`col-md-${i}`);
+
+        // Add class
+        $(`#${element}`).addClass(`col-md-${new_size}`);
+    });
 });
