8de8d2fe75d3a22b11b88836d2bd7b16800996c6
Jose F Rivas C <josefelixrc7@gmail.com>
2024-10-19 13:58:50 -0400

    feat: Add events to Add Forms Columns

--- assets/js/events/form/columns.js
+++ assets/js/events/form/columns.js
@@ -7,8 +7,31 @@ $(function()
         new wtools.OptionValue("0", "No", true)
         ,new wtools.OptionValue("1", "S&iacute;")
     ]);
-    //options_required.Build_('#component_columns_add select[name="required"]');
-    //options_required.Build_('#component_columns_modify select[name="required"]');
+    options_required.Build_('#component_columns_add select[name="required"]');
+    options_required.Build_('#component_columns_modify select[name="required"]');
+
+    let options_column_type = new wtools.SelectOptions();
+    const options_column_type_init = (options, callback) => new wtools.Request(server_config.current.api + "/forms/columns/types/read").Exec_((response_data) =>
+    {
+        try
+        {
+            let tmp_options = [];
+            for(let row of response_data.body.data)
+                tmp_options.push(new wtools.OptionValue(row.id, row.name));
+
+            options.options = tmp_options;
+            options.Build_('#component_columns_add select[name="id_column_type"]');
+            options.Build_('#component_columns_modify select[name="id_column_type"]');
+            callback();
+        }
+        catch(error)
+        {
+            new wtools.Notification('WARNING').Show_('No se pudo acceder a los tipos de columnas.');
+            new wtools.Notification('WARNING', 0, '#component_columns_add .notifications').Show_('No se pudo acceder a los tipos de columnas.');
+            new wtools.Notification('WARNING', 0, '#component_columns_modify .notifications').Show_('No se pudo acceder a los tipos de columnas.');
+        }
+    });
+    options_column_type_init(options_column_type, () => {})
 
     // Read columns
     const columns_read = () =>
@@ -75,5 +98,102 @@ $(function()
     };
     columns_read();
     $('#component_columns_read .update').click(() => columns_read());
+    
+    // Setup Avanced values in Add
+    $('#component_columns_add form select[name="id_column_type"]').change((e) =>
+    {
+        const value = $('#component_columns_add form select[name="id_column_type"]').val();
+        if(value == undefined)
+        {
+            new wtools.Notification('WARNING', 0, '#component_columns_add .notifications').Show_('Error al configurar los valores avanzados.');
+            return;
+        }
+        
+        if(value == "2" || value == "5" || value == "6" || value == "7" || value == "8")
+            $('#component_columns_add form input[name="length"]').val("");
+        else if(value == "3" || value == "9")
+            $('#component_columns_add form input[name="length"]').val("11");
+        else if(value == "4")
+            $('#component_columns_add form input[name="length"]').val("10,2");
+        else if(value == "1")
+            $('#component_columns_add form input[name="length"]').val("100");
+        else
+            $('#component_columns_add form input[name="length"]').val("");
+    });
+
+    // Click on Add Button
+    $('#component_columns_read .add').click(() => 
+    {
+        const add = () =>
+        {
+            $('#component_columns_add .notifications').html('');
+            $('#component_columns_add form select[name="id_column_type"]').val("1");
+            $('#component_columns_add form input[name="length"]').val("100");
+            $('#component_columns_add').modal('show');
+        }
+        options_column_type_init(options_column_type, add);
+    });
+
+    // Add
+    $('#component_columns_add form').submit((e) =>
+    {
+        e.preventDefault();
+
+        // Wait animation
+        let wait = new wtools.ElementState('#component_columns_add form button[type=submit]', true, 'button', new wtools.WaitAnimation().for_button);
+
+        // Form check
+        const check = new wtools.FormChecker(e.target).Check_();
+        if(!check)
+        {
+            $('#component_columns_add .notifications').html('');
+            wait.Off_();
+            new wtools.Notification('WARNING', 5000, '#component_columns_add .notifications').Show_('Hay campos inv&aacute;lidos.');
+            return;
+        }
+
+        // Get Form identifier
+        const url_params = new URLSearchParams(window.location.search);
+        const form_identifier = url_params.get('identifier');
+
+        if(form_identifier == undefined)
+        {
+            wait.Off_();
+            new wtools.Notification('WARNING').Show_('No se encontr&oacute; el identificador del formulario.');
+            return;
+        }
+
+        // Data collection
+        const data = new FormData($('#component_columns_add form')[0]);
+        data.append('form-identifier', form_identifier);
+
+        // Request
+        new wtools.Request(server_config.current.api + "/forms/columns/add", "POST", data, false).Exec_((response_data) =>
+        {
+            wait.Off_();
+
+            // Permissions error
+            if(response_data.status == 401)
+            {
+                new wtools.Notification('WARNING', 0, '#component_columns_add .notifications').Show_('No tiene permisos para acceder a este recurso.');
+                return;
+            }
+
+            // Notification Error
+            if(response_data.status != 200)
+            {
+                new wtools.Notification('WARNING', 0, '#component_columns_add .notifications').Show_('No se pudo crear la columna: ' + response_data.body.message);
+                return;
+            }
+
+            // Notifications
+            if(response_data.status == 200)
+            {
+                new wtools.Notification('SUCCESS').Show_('Columna creada exitosamente.');
+                $('#component_columns_add').modal('hide');
+                columns_read();
+            }
+        });
+    });
     
 });
