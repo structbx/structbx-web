1aa78cfeb1bf95626fdfb43712e87b6eeca97c88
Jose F Rivas C <josefelixrc7@gmail.com>
2024-11-02 09:51:04 -0400

    feat: Improve Header to go to settings

--- assets/js/elements/headers.js
+++ assets/js/elements/headers.js
@@ -32,7 +32,7 @@ class Headers
                             <li class="nav-item dropdown">
                                 <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
                                 <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    <li><a class="dropdown-item" href="../start/#settings">Configuraci&oacute;n</a></li>
+                                    <li><a class="dropdown-item go-settings-button" href="#">Configuraci&oacute;n</a></li>
                                     <li><a id="logout-button" class="dropdown-item" href="#">Cerrar sesi&oacute;n</a></li>
                                 </ul>
                             </li>
@@ -76,7 +76,7 @@ class Headers
                             <li class="nav-item dropdown">
                                 <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
                                 <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    <li><a class="dropdown-item" href="../start/#settings">Configuraci&oacute;n</a></li>
+                                    <li><a class="dropdown-item go-settings-button" href="#">Configuraci&oacute;n</a></li>
                                     <li><a id="logout-button" class="dropdown-item" href="#">Cerrar sesi&oacute;n</a></li>
                                 </ul>
                             </li>
@@ -124,7 +124,7 @@ class Headers
                             <li class="nav-item dropdown">
                                 <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
                                 <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    <li><a class="dropdown-item" href="../start/#settings">Configuraci&oacute;n</a></li>
+                                    <li><a class="dropdown-item go-settings-button" href="#">Configuraci&oacute;n</a></li>
                                     <li><a id="logout-button" class="dropdown-item" href="#">Cerrar sesi&oacute;n</a></li>
                                 </ul>
                             </li>
