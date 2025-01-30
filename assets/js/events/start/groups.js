497bed0c10d0db072e35c7556fe3231b8ad2683d
Jose F Rivas C <josefelixrc7@gmail.com>
2025-01-30 06:23:35 -0400

    feat: Add events to add groups

--- assets/js/events/start/groups.js
+++ assets/js/events/start/groups.js
@@ -40,4 +40,48 @@ $(function()
     };
     groups_read();
     
+    // Click on Add Group button
+    $('#component_groups_read .add').click(() => 
+    {
+        $('#component_groups_add').modal('show');
+    });
+
+    // Add Group
+    $('#component_groups_add form').submit((e) =>
+    {
+        e.preventDefault();
+
+        // Wait animation
+        let wait = new wtools.ElementState('#component_groups_add form button[type=submit]', true, 'button', new wtools.WaitAnimation().for_button);
+
+        // Form check
+        const check = new wtools.FormChecker(e.target).Check_();
+        if(!check)
+        {
+            $('#component_groups_add .notifications').html('');
+            wait.Off_();
+            new wtools.Notification('WARNING', 5000, '#component_groups_add .notifications').Show_('Hay campos inv&aacute;lidos.');
+            return;
+        }
+
+        // Data collection
+        const data = new FormData($('#component_groups_add form')[0]);
+
+        // Request
+        new wtools.Request(server_config.current.api + "/general/groups/add", "POST", data, false).Exec_((response_data) =>
+        {
+            wait.Off_();
+
+            // Manage response
+            const result = new ResponseManager(response_data, '#component_groups_add .notifications', 'Grupos: A&ntilde;adir');
+            if(!result.Verify_())
+                return;
+            
+            new wtools.Notification('SUCCESS').Show_('Grupo agregado exitosamente.');
+            groups_read();
+            wtools.CleanForm($('#component_groups_add form'));
+            $('#component_groups_add').modal('hide');
+        });
+    });
+    
 });
