97c7f858d9eb65dbb92f6e12ee438e4a0abd3169
Jose F Rivas C <josefelixrc7@gmail.com>
2025-01-30 07:20:48 -0400

    feat: Add events to modify groups

--- assets/js/events/start/groups.js
+++ assets/js/events/start/groups.js
@@ -83,5 +83,86 @@ $(function()
             $('#component_groups_add').modal('hide');
         });
     });
+
+    // Read group to modify
+    $(document).on("click", '#component_groups_read table tbody tr', (e) =>
+    {
+        e.preventDefault();
+
+        // Wait animation
+        let wait = new wtools.ElementState('#wait_animation_page', true, 'block', new wtools.WaitAnimation().for_page);
+
+        // Get ID
+        let id = $(e.currentTarget).attr('group-id');
+        if(id == undefined)
+        {
+            wait.Off_();
+            new wtools.Notification('WARNING').Show_('No se encontr&oacute; el identificador de grupo.');
+            return;
+        }
+
+        // Read user to modify
+        new wtools.Request(server_config.current.api + `/general/groups/read/id?id=${id}`).Exec_((response_data) =>
+        {
+            // Manage response
+            const result = new ResponseManager(response_data, '', 'Grupos: Modificar');
+            if(!result.Verify_())
+                return;
+
+            // Handle no results or zero results
+            if(response_data.body.data.length < 1)
+            {
+                new wtools.Notification('SUCCESS').Show_('Sin resultados.');
+                return;
+            }
+
+            wtools.CleanForm($('#component_groups_read form'));
+
+            // Set data
+            $('#component_groups_modify input[name="id"]').val(response_data.body.data[0].id);
+            $('#component_groups_modify input[name="group"]').val(response_data.body.data[0].group);
+
+            wait.Off_();
+            $('#component_groups_modify').modal('show');
+        });
+    });
+    
+    // Modify Group
+    $('#component_groups_modify form').submit((e) =>
+    {
+        e.preventDefault();
+
+        // Wait animation
+        let wait = new wtools.ElementState('#component_groups_modify form button[type=submit]', true, 'button', new wtools.WaitAnimation().for_button);
+
+        // Form check
+        const check = new wtools.FormChecker(e.target).Check_();
+        if(!check)
+        {
+            $('#component_groups_modify .notifications').html('');
+            wait.Off_();
+            new wtools.Notification('WARNING', 5000, '#component_groups_modify .notifications').Show_('Hay campos inv&aacute;lidos.');
+            return;
+        }
+
+        // Data collection
+        const data = new FormData($('#component_groups_modify form')[0]);
+
+        // Request
+        new wtools.Request(server_config.current.api + "/general/groups/modify", "PUT", data, false).Exec_((response_data) =>
+        {
+            wait.Off_();
+
+            // Manage response
+            const result = new ResponseManager(response_data, '#component_groups_modify .notifications', 'Grupos: Modificar');
+            if(!result.Verify_())
+                return;
+            
+            new wtools.Notification('SUCCESS').Show_('Grupo modificado exitosamente.');
+            groups_read();
+            wtools.CleanForm($('#component_groups_modify form'));
+            $('#component_groups_modify').modal('hide');
+        });
+    });
     
 });
