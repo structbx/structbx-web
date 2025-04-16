699987dceaa6b6738c8e2c379913bbda636798d2
Jose F Rivas C <josefelixrc7@gmail.com>
2025-04-15 22:30:07 -0400

    feat: Add events to add, modify and delete views

--- assets/js/events/form/views.js
+++ assets/js/events/form/views.js
@@ -72,8 +72,8 @@ $(function()
                             ${row.name}
                         </a>
                         <div class="py-2 pe-4 btn-group" role="group">
-                            <button type="button" class="btn btn-sm btn-secondary modify" view-id="${row.id}"><i class="fas fa-pen"></i></button>
-                            <button type="button" class="btn btn-sm btn-secondary delete" view-id="${row.id}"><i class="fas fa-trash"></i></button>
+                            <button type="button" class="btn btn-sm btn-secondary modify" view-id="${row.id}" view-name="${row.name}"><i class="fas fa-pen"></i></button>
+                            <button type="button" class="btn btn-sm btn-secondary delete" view-id="${row.id}" view-name="${row.name}"><i class="fas fa-trash"></i></button>
                         </div>
                     </div>
                 `;
@@ -131,16 +131,174 @@ $(function()
     {
         $('#component_data_views_add').modal('show');
     });
+    
+    $('#component_data_views_add form').submit((e) =>
+    {
+        e.preventDefault();
+
+        // Wait animation
+        let wait = new wtools.ElementState('#component_data_views_add form button[type=submit]', true, 'button', new wtools.WaitAnimation().for_button);
+
+        // Form check
+        const check = new wtools.FormChecker(e.target).Check_();
+        if(!check)
+        {
+            $('#component_data_views_add .notifications').html('');
+            wait.Off_();
+            new wtools.Notification('WARNING', 5000, '#component_data_views_add .notifications').Show_('Hay campos inv&aacute;lidos.');
+            return;
+        }
+
+        // Get Form identifier
+        const form_identifier = wtools.GetUrlSearchParam('identifier');
+        if(form_identifier == undefined)
+        {
+            wait.Off_();
+            new wtools.Notification('WARNING').Show_('No se encontr&oacute; el identificador del formulario.');
+            return;
+        }
+
+        // Data collection
+        const data = new FormData($('#component_data_views_add form')[0]);
+        data.append('conditions', wtools.GetUrlSearchParam('conditions'));
+        data.append('order', wtools.GetUrlSearchParam('order'));
+        data.append('form-identifier', form_identifier);
+
+        // Request
+        new wtools.Request(server_config.current.api + "/forms/views/add", "POST", data, false).Exec_((response_data) =>
+        {
+            wait.Off_();
+
+            // Manage response
+            const result = new ResponseManager(response_data, '#component_data_views_add .notifications', 'Vistas: A&ntilde;adir');
+            if(!result.Verify_())
+                return;
+
+            new wtools.Notification('SUCCESS').Show_('Vista creada exitosamente.');
+            $('#component_data_views_add').modal('hide');
+            views_read();
+        });
+    });
 
     // Modify a view
-    $(document).on('click', '#component_data_views .contents .modify', () => 
+    $(document).on('click', '#component_data_views .contents .modify', (e) => 
     {
+        const view_id = $(e.currentTarget).attr('view-id');
+        const view_name = $(e.currentTarget).attr('view-name');
+        if(view_id == undefined || view_name == undefined)
+        {
+            new wtools.Notification('WARNING').Show_('No se encontr&oacute; la vista.');
+            return;
+        }
+        $('#component_data_views_modify input[name="name"]').val(view_name);
+        $('#component_data_views_modify input[name="id"]').val(view_id);
         $('#component_data_views_modify').modal('show');
     });
 
-    // Delete a view
-    $(document).on('click', '#component_data_views .contents .delete', () => 
+    $('#component_data_views_modify form').submit((e) =>
     {
+        e.preventDefault();
+
+        // Wait animation
+        let wait = new wtools.ElementState('#component_data_views_modify form button[type=submit]', true, 'button', new wtools.WaitAnimation().for_button);
+
+        // Form check
+        const check = new wtools.FormChecker(e.target).Check_();
+        if(!check)
+        {
+            $('#component_data_views_modify .notifications').html('');
+            wait.Off_();
+            new wtools.Notification('WARNING', 5000, '#component_data_views_modify .notifications').Show_('Hay campos inv&aacute;lidos.');
+            return;
+        }
+
+        // Get Form identifier
+        const form_identifier = wtools.GetUrlSearchParam('identifier');
+        if(form_identifier == undefined)
+        {
+            wait.Off_();
+            new wtools.Notification('WARNING').Show_('No se encontr&oacute; el identificador del formulario.');
+            return;
+        }
+
+        // Data collection
+        const data = new FormData($('#component_data_views_modify form')[0]);
+        data.append('conditions', wtools.GetUrlSearchParam('conditions'));
+        data.append('order', wtools.GetUrlSearchParam('order'));
+        data.append('form-identifier', form_identifier);
+
+        // Request
+        new wtools.Request(server_config.current.api + "/forms/views/modify", "PUT", data, false).Exec_((response_data) =>
+        {
+            wait.Off_();
+
+            // Manage response
+            const result = new ResponseManager(response_data, '#component_data_views_modify .notifications', 'Vistas: Modificar');
+            if(!result.Verify_())
+                return;
+
+            new wtools.Notification('SUCCESS').Show_('Vista modificada exitosamente.');
+            $('#component_data_views_modify').modal('hide');
+            views_read();
+        });
+    });
+
+    // Delete a view
+    $(document).on('click', '#component_data_views .contents .delete', (e) => 
+    {
+        const view_id = $(e.currentTarget).attr('view-id');
+        const view_name = $(e.currentTarget).attr('view-name');
+        if(view_id == undefined || view_name == undefined)
+        {
+            new wtools.Notification('WARNING').Show_('No se encontr&oacute; la vista.');
+            return;
+        }
+        $('#component_data_views_delete .name').text(view_name);
+        $('#component_data_views_delete input[name="id"]').val(view_id);
         $('#component_data_views_delete').modal('show');
-    });    
+    });
+
+    $('#component_data_views_delete form').submit((e) =>
+    {
+        e.preventDefault();
+
+        // Wait animation
+        let wait = new wtools.ElementState('#component_data_views_delete form button[type=submit]', true, 'button', new wtools.WaitAnimation().for_button);
+
+        // Form check
+        const check = new wtools.FormChecker(e.target).Check_();
+        if(!check)
+        {
+            $('#component_data_views_delete .notifications').html('');
+            wait.Off_();
+            new wtools.Notification('WARNING', 5000, '#component_data_views_delete .notifications').Show_('Hay campos inv&aacute;lidos.');
+            return;
+        }
+
+        // Get Form identifier
+        const form_identifier = wtools.GetUrlSearchParam('identifier');
+        if(form_identifier == undefined)
+        {
+            wait.Off_();
+            new wtools.Notification('WARNING').Show_('No se encontr&oacute; el identificador del formulario.');
+            return;
+        }
+
+        // Request
+        const view_id = $('#component_data_views_delete input[name="id"]').val();
+        new wtools.Request(server_config.current.api + `/forms/views/delete?id=${view_id}&form-identifier=${form_identifier}`, "DEL").Exec_((response_data) =>
+        {
+            wait.Off_();
+
+            // Manage response
+            const result = new ResponseManager(response_data, '#component_data_views_delete .notifications', 'Vistas: Modificar');
+            if(!result.Verify_())
+                return;
+
+            new wtools.Notification('SUCCESS').Show_('Vista eliminada exitosamente.');
+            $('#component_data_views_delete').modal('hide');
+            views_read();
+            read_current_view();
+        });
+    });
 });
