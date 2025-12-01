a438648a03e2e3f115be1f5b3748890ba1f84af2
Jose F Rivas C <josefelixrc7@gmail.com>
2025-12-01 17:17:38 -0400

    feat: Improve Footer

--- assets/js/elements/footers.js
+++ assets/js/elements/footers.js
@@ -14,9 +14,13 @@ class Footers
                 <div class="d-flex align-items-center justify-content-between small">
                     <div class="text-muted">Copyright &copy; ${year} StructBX.</div>
                     <div>
-                        <a href="#">Pol&iacute;tica de privacidad</a>
+                        <a target="_blank" href="https://structbx.com">structbx.com</a>
                         &middot;
-                        <a href="#">T&eacute;rminos y condiciones</a>
+                        <a target="_blank" href="https://structbx.com/assets/files/terms_and_conditions.pdf">Pol&iacute;tica de privacidad</a>
+                        &middot;
+                        <a target="_blank" href="https://structbx.com/assets/files/privacy_policy.pdf">T&eacute;rminos y condiciones</a>
+                        &middot;
+                        <a target="_blank" href="https://www.apache.org/licenses/LICENSE-2.0">Licencia Apache 2.0</a>
                     </div>
                 </div>
             </div>
