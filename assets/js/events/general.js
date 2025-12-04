47ad968b2eb5480d7769bb4808cc7d5c36db710d
Jose F Rivas C <josefelixrc7@gmail.com>
2025-12-04 16:45:13 -0400

    feat: Add previous logout and navigation button handlers for improved user experience

--- assets/js/events/general.js
+++ assets/js/events/general.js
@@ -6,4 +6,35 @@ $(function ()
     databases_read_id();
     
     username_logued_read();
+
+    $(document).on('click', '#logout-button', (e) =>
+    {
+        e.preventDefault();
+
+        logout();
+    });
+    
+    $(document).on('click', '.go-button', function(e)
+    {
+        e.preventDefault();
+        let path = $(e.currentTarget).attr('go-path');
+        let hash = $(e.currentTarget).attr('go-hash');
+        if(window.location.pathname == path || window.location.pathname == path + "/")
+        {
+            location.hash = hash;
+            location.reload();
+        }
+        else
+        {
+            new wtools.ElementState('#wait_animation_page', true, 'block', new wtools.WaitAnimation().for_page);
+            window.location.href = path + hash;
+        }
+    });
+
+    $(document).on('click', '.go-form-button', function(e)
+    {
+        e.preventDefault();
+        new wtools.ElementState('#wait_animation_page', true, 'block', new wtools.WaitAnimation().for_page);
+        window.location.href = `/table?identifier=${wtools.GetUrlSearchParam('identifier')}`;
+    });
 });
